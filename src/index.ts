72bd452263b0004c0e0774934219607ae1b1213f
chikachan <admin@chikachan.com>
2025-06-23 15:58:50 +0900

    メンションによる害悪化、毎日4時3分の通知追加

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import { Button, Components, DiscordHono, _channels_$_messages } from 'discord-h
 const app = new DiscordHono()
   .cron('43 * * * *', async c => {
 	await c.rest('POST', _channels_$_messages, ['1386038293891911700'], {
-		content: '43だよ.'
+		content: '@everyone 43だよ.'
+	})
+  })
+  .cron('3 4 * * *', async c => {
+	await c.rest('POST', _channels_$_messages, ['1386038293891911700'], {
+		content: '@everyone 43だよ.'
 	})
   })
   .command('43', c => c.res('43だよ.'))
